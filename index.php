5894a27dd99551705babbf6bb652f86258dadd91
marineco <marinecolonge@gmail.com>
2017-01-28 21:02:46 +0100

    ajout de Twig + test de Twig

--- index.php
+++ index.php
@@ -4,6 +4,8 @@ require_once 'vendor/autoload.php';
 
 $faker = Faker\Factory::create();
 
+echo 'Test Faker : <br>';
+
 echo $faker->name, "<br>";
 
 echo $faker->address, "<br>";
@@ -13,4 +15,11 @@ echo $faker->text, "<br>";
 for ($i = 0; $i < 10; $i++) {
 	echo $faker->name, "<br>";
 }
+
+$loader = new Twig_Loader_Array(array('index' => '<div>Test Twig => Hello {{ name }}!</div>',));
+
+$twig = new Twig_Environment($loader);
+
+echo $twig->render('index', array('name' => 'Marine'));
+
 ?>
